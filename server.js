711f8cddc0b7a7e6d78cbcd1e45660931e6b562d
Kofoworola Olabisi <toyosio@gmail.com>
2018-03-04 20:16:21 -0600

    commit

--- server.js
+++ server.js
@@ -1,37 +1,44 @@
 'use strict';
 
 // Modules
-const express    = require('express'),
-      exphbs     = require('express-handlebars'),
-      bodyParser = require('body-parser'),
-      logger     = require('morgan'),
-      mongoose   = require('mongoose'),
-      Promise    = require('bluebird'),
-      cheerio    = require('cheerio'),
-      rp         = require('request-promise'),
+const express = require('express'),
+  exphbs = require('express-handlebars'),
+  bodyParser = require('body-parser'),
+  logger = require('morgan'),
+  mongoose = require('mongoose'),
+  Promise = require('bluebird'),
+  cheerio = require('cheerio'),
+  rp = require('request-promise'),
 
-      // Local dependencies
-      Article    = require('./models/Article.js'),
-      Comment    = require('./models/Comment.js'),
+  // Local dependencies
+  Article = require('./models/Article.js'),
+  Comment = require('./models/Comment.js'),
 
-      // Const vars
-      app    = express(),
-      hbs    = exphbs.create({ defaultLayout: 'main', extname: '.hbs' }),
-      PORT   = process.env.PORT || 3000,
-      DB_URI = process.env.MONGODB_URI || require('./mongodb_uri.json').LOCAL_URI;
+  // Const vars
+  app = express(),
+  hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: '.hbs'
+  }),
+  PORT = process.env.PORT || 3000,
+  DB_URI = process.env.MONGODB_URI || require('./mongodb_uri.json').LOCAL_URI;
 
 // Handlebars init
 app.engine('.hbs', hbs.engine);
 app.set('view engine', '.hbs');
-if (process.env.PORT) app.enable('view cache');  // Disable view cache for local testing
+if (process.env.PORT) app.enable('view cache'); // Disable view cache for local testing
 
 // Morgan for logging
 app.use(logger('dev'));
 
 // Body parser init
 app.use(bodyParser.json());
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({
+  type: 'application/vnd.api+json'
+}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(bodyParser.text());
 
 // Route for static content
@@ -56,25 +63,25 @@ db.once('open', function () {
 app.get('/', (req, res) => {
   rp('http://www.nytimes.com/pages/todayspaper').then(html => {
     const $ = cheerio.load(html),
-          promises = [];
+      promises = [];
 
-    $('h3').each(function(i, element) {
-      const link    = $(element).find('a').attr('href'),
-            title   = $(element).find('a').text().trim(),
-            summary = $(element).next().next().text().trim();
+    $('h3').each(function (i, element) {
+      const link = $(element).find('a').attr('href'),
+        title = $(element).find('a').text().trim(),
+        summary = $(element).next().next().text().trim();
 
       if (title) {
         promises.unshift(new Promise((resolve, reject) => {
-          Article.update(
-            { link: link },   // only create new entry if link does not exist in Articles
-            { $setOnInsert:
-              {
+          Article.update({
+              link: link
+            }, // only create new entry if link does not exist in Articles
+            {
+              $setOnInsert: {
                 link: link,
                 title: title,
                 summary: summary
               }
-            },
-            {
+            }, {
               upsert: true,
               setDefaultsOnInsert: true
             }
@@ -87,8 +94,12 @@ app.get('/', (req, res) => {
 
     // When all updates are resolved, continue
     Promise.all(promises).then(() =>
-      Article.find({}).sort({ date: 1 }).limit(1).populate('comments').exec((err, doc) =>
-        res.render('index', {article: doc[0]})
+      Article.find({}).sort({
+        date: 1
+      }).limit(1).populate('comments').exec((err, doc) =>
+        res.render('index', {
+          article: doc[0]
+        })
       )
     );
   });
@@ -97,7 +108,9 @@ app.get('/', (req, res) => {
 
 // Additional routes
 app.get('/articles', (req, res) => {
-  Article.find({}).sort({ date: 1 }).limit(10).populate('comments').exec((err, docs) =>
+  Article.find({}).sort({
+    date: 1
+  }).limit(10).populate('comments').exec((err, docs) =>
     res.json(docs)
   )
 });
@@ -109,11 +122,16 @@ app.get('/comments/:id', (req, res) => {
 });
 
 app.post('/', (req, res) => {
-  Comment.create({comment: req.body.comment}).then(comment => {
+  Comment.create({
+    comment: req.body.comment
+  }).then(comment => {
     console.log(comment);
     Article.findByIdAndUpdate(
-      req.body.id,
-      { $push: { "comments": comment._id } }
+      req.body.id, {
+        $push: {
+          "comments": comment._id
+        }
+      }
     ).then(() =>
       res.json(comment)
     )
@@ -126,7 +144,9 @@ app.delete('/', (req, res) => {
 
     for (const id of article.comments) {
       promises.push(new Promise((resolve, reject) => {
-        Comment.remove({ _id: id}).then(data => resolve(data));
+        Comment.remove({
+          _id: id
+        }).then(data => resolve(data));
       }));
     }
 
